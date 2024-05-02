d13c55a8e2fb3e3582b1ebdb94e3ceca6c87b991
harishkumark96 <harishkumark05@gmail.com>
2024-05-02 21:22:31 +0530

    after update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { SocketService } from './service/socket.service';
 import {NodeDataService} from './service/node-data.service';
+import {PersonalDataService} from './service/personal-data.service';
+import { AuthService} from  './auth/auth.service';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CapitalPipe } from './pipes/capital.pipe';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
-import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './portfolio/login/login.component';
+import { ProfileComponent } from './portfolio/profile/profile.component';
+import { AboutComponent } from './portfolio/about/about.component';
+import { SkillsComponent } from './portfolio/skills/skills.component';
+import { ResumeComponent } from './portfolio/resume/resume.component';
+import { ContactComponent } from './portfolio/contact/contact.component';
+import { MainComponent } from './portfolio/main/main.component';
 
 @NgModule({
   declarations: [
@@ -20,17 +31,26 @@ import { ProfileComponent } from './profile/profile.component';
     DashboardComponent,
     CapitalPipe,
     BarChartComponent,
-    ProfileComponent
+    LoginComponent,
+    ProfileComponent,
+    AboutComponent,
+    SkillsComponent,
+    ResumeComponent,
+    ContactComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule,
     NgxChartsModule,
-    HttpClientModule
+    HttpClientModule,FormsModule,ReactiveFormsModule
   ],
   providers: [
     SocketService,
-    NodeDataService
+    NodeDataService,
+    PersonalDataService,
+    AuthService
     ],
   bootstrap: [AppComponent]
 })
